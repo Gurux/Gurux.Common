46441817502a0f5b31638cd567e91c7b773f53f5
gurux01 <gurux@gurux.fi>
2016-11-24 11:16:28 +0200

    The component is signed

--- Development/Properties/AssemblyInfo.cs
+++ Development/Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Resources;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("88e5545d-d8d3-4a4b-9b68-231496ea8432")]
-[assembly: AssemblyVersion("8.0.8.4")]
-[assembly: AssemblyFileVersion("8.0.8.4")]
+[assembly: AssemblyVersion("8.0.8.5")]
+[assembly: AssemblyFileVersion("8.0.8.5")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
